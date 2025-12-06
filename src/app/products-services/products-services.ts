741022d93be6d7363f3d3eabd8bd9a62656d2b11
yambaolr <yambaolorinrobelle@gmail.com>
2025-12-06 20:54:54 +0800

    📎 test

--- src/app/products-services/products-services.ts
+++ src/app/products-services/products-services.ts
@@ -26,12 +26,6 @@ export class ProductsServices {
       description: 'Daily baked selection of cakes, cookies, and other sweet treats to pair with your coffee.',
       price: '₱60 - ₱120',
       image: 'https://cdn.pixabay.com/photo/2016/02/29/00/19/cake-1227842_1280.jpg',
-    },
-    {
-      name: 'Jann Carl Dungo1',
-      description: 'Daily baked selection of cakes, cookies, and other sweet treats to pair with your coffee.',
-      price: '₱60 - ₱120',
-      image: 'https://cdn.pixabay.com/photo/2016/02/29/00/19/cake-1227842_1280.jpg',
     }
   ];
 }
